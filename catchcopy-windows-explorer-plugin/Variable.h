cd0106c7668e0616e62e9015441fa9e71271702a
BRULE Herman <alpha_one_x86@first-world.info>
2013-05-11 18:32:38 +0200

    Split the menu string

--- catchcopy-windows-explorer-plugin/Variable.h
+++ catchcopy-windows-explorer-plugin/Variable.h
@@ -3,5 +3,6 @@
 
 /// \brief Un-comment this next line to put ultracopier in debug mode
 //#define CATCHCOPY_EXPLORER_PLUGIN_DEBUG
+#define CATCHCOPY_EXPLORER_PLUGIN_SOFTWARE_NAME "Ultracopier"
 
 #endif // VARIABLE_H
